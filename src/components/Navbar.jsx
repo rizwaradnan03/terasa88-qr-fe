39cc379c151a2d1a2a2947e259d275aea5ef2d45
Rizwar <rizwar.adnanbyantara@gmail.com>
2023-07-17 10:48:18 +0700

    Penambahan Garis

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="container-fluid bg-danger p-2">
+        <h2 className="text-center text-light">Teras Nusantara 88</h2>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
